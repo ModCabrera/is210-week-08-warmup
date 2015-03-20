4e32b1ff97866a2d01e2f47aedc077b7cdee4313
modestocabrera <modesto.cabrera@spsmail.cuny.edu>
2015-03-20 20:35:14 +0000

    test

--- task_03.py
+++ task_03.py
@@ -3,22 +3,13 @@
 """Module Contains a Function for Lexicographical Analysis"""
 
 def lexicographics(to_analyze):
-    lines = ''
-    line2  = ''
-    line3 = ''
-    totalperline = 0
-    total_lines = []
-    lista_t= []
-    for char in to_analyze:
-        lines += char
-        line2 = lines.split('\n')
-        line3 = line2[0]
-        line3.split()
-        lista_t = line3.split()
-        totalperline = len(lista_t)
-    total_lines.append(totalperline)
-    return total_lines
+    splitsent = to_analyze.split('\n')
+    splitwords = to_analyze.split()
+    maximum = max(len(splitsent),len(splitwords))
+    minimum = min(len(splitsent),len(splitsent))
+    average = float(len(splitwords)/len(splitsent)) 
+    print maximum, minimum, average
 
 if __name__ == '__main__':
-    print lexicographics('''Don't stop believing,
+    lexicographics('''Don't stop believing,
     Hold on to that feeling.''')
