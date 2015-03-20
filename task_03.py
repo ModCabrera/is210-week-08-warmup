c1b27db940fa66f87bfb98fb5b9269fa0aaaa9af
modestocabrera <modesto.cabrera@spsmail.cuny.edu>
2015-03-20 02:05:34 +0000

    question

--- task_03.py
+++ task_03.py
@@ -4,15 +4,13 @@
 
 
 def lexicographics(to_analyze):
-    item = ''
-    maximum = 0
-    minimum = 0
-    avg = 0
-    for i in to_analyze:
-        item += i
-        item.split('\n')
-    item = [item]
-    print item
- 
+    lines = ''
+    lineslist=[]
+    for char in to_analyze:
+        lines += char
+        lineslist = (lines.split('\n'))
+    print lineslist
+    
 if __name__ == '__main__':
-    print lexicographics('''Don't stop believing,\n Hold on to that feeling.''')
+    print lexicographics('''Don't stop believing,
+Hold on to that feeling.''')
