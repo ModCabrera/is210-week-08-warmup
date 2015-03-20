250bb34dae6c589b93a0ad26e062044efffcef98
modestocabrera <modesto.cabrera@spsmail.cuny.edu>
2015-03-20 21:58:28 +0000

    test3

--- task_03.py
+++ task_03.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Module Contains a Function for Lexicographical Analysis"""
+import decimal
 
 def lexicographics(to_analyze):
-    splitsent = to_analyze.split('\n')
-    splitwords = to_analyze.split()
-    maximum = max(len(splitsent),len(splitwords))
-    minimum = min(len(splitsent),len(splitsent))
-    average = float(len(splitwords)/len(splitsent)) 
-    print maximum, minimum, average
+    list_of_sentences = to_analyze.split('\n')
+    max_words = 0
+    min_words = len(list_of_sentences[0].split())
+    total_words = 0
+    for sentence in list_of_sentences:
+        numwords = len(sentence.split())
+        if numwords > max_words:
+            max_words = numwords
+            
+        if numwords <= min_words:
+            min_words = numwords
+        total_words += numwords
+    average = total_words/decimal.Decimal(len(list_of_sentences))
+    return max_words, min_words, average
 
 if __name__ == '__main__':
-    lexicographics('''Don't stop believing,
-    Hold on to that feeling.''')
+    import data
+    print lexicographics(data.SHAKESPEARE)
